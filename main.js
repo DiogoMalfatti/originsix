6cbcdf5c76c165501bc350939b5c12e885095a1c
Diogo Malfatti <diimalfatti@gmail.com>
2021-06-23 13:53:50 -0300

    criado a pagina depoimentos e contato

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// ===== MENU NAV ===== ===== ===== =====
 const nav = document.querySelector('.header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -14,7 +15,9 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+//  / MENU NAV ===== ===== ===== =====
 
+//  ===== SCROLL NAV ===== ===== ===== =====
 const header = document.querySelector('.header')
 const navHeight = header.offsetHeight
 
@@ -25,3 +28,33 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+//  / SCROLL NAV ===== ===== ===== =====
+
+//  ===== TESTIMONIALS CAROUSSEL ===== ===== ===== =====
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  mousewheel: true,
+  keyboard: true,
+})
+//  / TESTIMONIALS CAROUSSEL ===== ===== ===== =====
+
+//  ===== CONTACT SCROLL REVEAL ===== ===== ===== =====
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true,
+})
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links
+  `,
+  { interval: 100 },
+)
+//  / CONTACT SCROLL REVEAL ===== ===== ===== =====
