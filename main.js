71e9132d39f9cdcb48865b11fb9002bc35a8fb86
Diogo Malfatti <diimalfatti@gmail.com>
2021-06-25 11:53:49 -0300

    feito media queries e finalizado

--- main.js
+++ main.js
@@ -18,10 +18,10 @@ for (const link of links) {
 //  / MENU NAV ===== ===== ===== =====
 
 //  ===== SCROLL NAV ===== ===== ===== =====
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('.header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('.header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -38,6 +38,12 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
 })
 //  / TESTIMONIALS CAROUSSEL ===== ===== ===== =====
 
@@ -61,8 +67,9 @@ scrollReveal.reveal(
 //  / CONTACT SCROLL REVEAL ===== ===== ===== =====
 
 //  BACK-TO-TOP SCROLL ===== ===== ===== =====
+const backToTopButton = document.querySelector('.back-to-top')
+
 function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
   if (window.scrollY > 560) {
     backToTopButton.classList.add('show')
   } else {
@@ -71,9 +78,37 @@ function backToTop() {
 }
 //  / BACK-TO-TOP SCROLL ===== ===== ===== =====
 
+//  MENU NAV ATIVO ===== ===== ===== =====
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+//  / MENU NAV ATIVO ===== ===== ===== =====
+
 //  SCROLL ===== ===== ===== =====
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   backToTop()
+  activateMenuAtCurrentSection()
 })
 //  / SCROLL ===== ===== ===== =====
